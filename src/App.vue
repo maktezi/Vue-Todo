f2568caeb63f40d5346e4045542f963c1e3bf6af
marknierras <mlcnierras@gmail.com>
2024-08-11 11:57:19 +0800

    base

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<main class="flex m-auto justify-center items-center h-dvh">
+  <div class="flex-col border-2 p-4 rounded-2xl">
+    <p class="text-gray-100 text-2xl font-bold text-center mb-4">Todo List</p>
+    <section>
+      <p class="text-gray-100">
+        What's up, <input type="text" placeholder="Name here" v-model="name" class="p-2 text-gray-100 border-b bg-transparent">
+      </p>
+    </section>
+  </div>
+</main>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref('')
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return a.createdAt - b.createdAt
+}))
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name')
+})
+</script>
